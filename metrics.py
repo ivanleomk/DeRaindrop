f5dece8291d69df1721be09b3c6638e3d86c01d9
Ivan Leo <ivanleomk@gmail.com>
2021-09-15 15:05:41 +0800

    Trying to work by renaming the package imports

--- metrics.py
+++ metrics.py
@@ -1,14 +1,14 @@
 import skimage
 import cv2
-from skimage.metrics import peak_noise_signal_ratio,structural_similarity
+from skimage.metrics import compare_psnr,compare_ssim
 
 
 def calc_psnr(im1, im2):
     im1_y = cv2.cvtColor(im1, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
     im2_y = cv2.cvtColor(im2, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
-    return peak_noise_signal_ratio(im1_y, im2_y)
+    return compare_psnr(im1_y, im2_y)
 
 def calc_ssim(im1, im2):
     im1_y = cv2.cvtColor(im1, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
     im2_y = cv2.cvtColor(im2, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
-    return structural_similarity(im1_y, im2_y)
+    return compare_ssim(im1_y, im2_y)
